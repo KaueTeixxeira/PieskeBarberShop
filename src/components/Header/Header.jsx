48a4f14576a7e11954a3d5d06211319cd83f148f
KaueTeixxeira <kauervfteixeira@gmail.com>
2025-05-01 14:06:14 -0300

    Initial setup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+
+const Header = () => {
+    return (
+        <header className="bg-[#E0E1DD] h-[100px] flex justify-center items-center">
+            <img
+                src="/src/assets/images/header/logoSemFundoAjustada.png"
+                alt="Logo da cidade"
+                className="h-[80px] object-cover"
+            />
+        </header>
+    );
+};
+
+export default Header;
